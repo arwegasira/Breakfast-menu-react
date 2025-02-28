e3e360cba723049b4d643148a780286097b3cd38
arwegasira <ac.rwegasira@gmail.com>
2025-02-28 08:07:11 +0200

    login yup validation rule

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -11,6 +11,16 @@ export const roomValidations = Yup.object({
   phoneNumber: Yup.string().required('Required'),
 })
 
+const passwordRule = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+export const loginValidations = Yup.object({
+  email: Yup.string().email('Invalid email').required('Email is Required'),
+  password: Yup.string()
+    .required('password is Required')
+    .matches(passwordRule, {
+      message:
+        'Password must be at least 8 char long, contains at least one uppercase, at least one lowercase, and at least one digit',
+    }),
+})
 export const flattenPolyFill = (arr, dep = 1) => {
   return dep > 0
     ? arr.reduce((acc, curr) => {
