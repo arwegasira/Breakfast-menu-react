d79f6555724a6bc0780dc0de7b3c6ec34214b42e
arwegasira <ac.rwegasira@gmail.com>
2025-02-28 08:06:02 +0200

    login page jsx structure

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,48 @@
+import { Formik, Form } from 'formik'
+import FormIkInput from '../Components/FormIk/FormIkInput'
+import { loginValidations } from '../Utils'
+import { FcGoogle } from 'react-icons/fc'
 const Login = () => {
-  return <h2>Login</h2>
+  return (
+    <section>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+        validationSchema={loginValidations}
+      >
+        {({ isSubmitting }) => {
+          return (
+            <Form>
+              <FormIkInput
+                label='Email'
+                name='email'
+                type='email'
+                id='email'
+              ></FormIkInput>
+              <FormIkInput
+                label='Password'
+                name='password'
+                type='password'
+                id='password'
+              ></FormIkInput>
+              <div>
+                <button type='submit' disabled={isSubmitting}>
+                  Submit
+                </button>
+              </div>
+            </Form>
+          )
+        }}
+      </Formik>
+      <div>
+        <button type='button'>
+          <FcGoogle />
+          <span>Sign with Google</span>
+        </button>
+      </div>
+    </section>
+  )
 }
 export default Login
