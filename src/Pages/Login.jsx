1ec03cf146dbabce60257d4727fe8775484ccff6
arwegasira <ac.rwegasira@gmail.com>
2025-03-01 11:25:31 +0200

    login page styling

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,7 +4,10 @@ import { loginValidations } from '../Utils'
 import { FcGoogle } from 'react-icons/fc'
 const Login = () => {
   return (
-    <section>
+    <section className='flex flex-col justify-center h-screen w-[90%] max-w-[42rem] mx-auto'>
+      <h2 className='text-xl md:text-2xl lg:text-3xl font-medium lg:font-semibold'>
+        Welcome Back!
+      </h2>
       <Formik
         initialValues={{ email: '', password: '' }}
         onSubmit={(values) => {
@@ -14,32 +17,40 @@ const Login = () => {
       >
         {({ isSubmitting }) => {
           return (
-            <Form>
+            <Form className=''>
               <FormIkInput
                 label='Email'
                 name='email'
                 type='email'
                 id='email'
+                placeholder='Email'
+                size='input-md'
               ></FormIkInput>
               <FormIkInput
                 label='Password'
                 name='password'
                 type='password'
                 id='password'
+                placeholder='Password'
+                size='input-md'
               ></FormIkInput>
-              <div>
-                <button type='submit' disabled={isSubmitting}>
-                  Submit
+              <div className='mt-6 flex justify-center'>
+                <button
+                  type='submit'
+                  disabled={isSubmitting}
+                  className='btn primary-button w-full'
+                >
+                  Login
                 </button>
               </div>
             </Form>
           )
         }}
       </Formik>
-      <div>
-        <button type='button'>
-          <FcGoogle />
-          <span>Sign with Google</span>
+      <div className='mt-6'>
+        <button type='button' className='flex justify-center w-full gap-4 btn'>
+          <FcGoogle className='text-2xl md:text-3xl' />
+          <span className=' text-md md:text-lg'>Sign with Google</span>
         </button>
       </div>
     </section>
