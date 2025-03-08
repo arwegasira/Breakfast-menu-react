c2e40c58fb347b08d726b00ec0257f885536e984
arwegasira <ac.rwegasira@gmail.com>
2025-03-08 22:24:34 +0200

    redirect to home or orders

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,4 +1,21 @@
+import { redirect } from 'react-router-dom'
+
+export const loader = async ({ request }) => {
+  const url = new URL(request.url)
+  const params = Object.fromEntries(url.searchParams)
+  if (params.googleSignRedirect) {
+    const userData = JSON.parse(params.user)
+    localStorage.setItem('user', JSON.stringify(userData))
+  }
+  let user = localStorage.getItem('user')
+  user = JSON.parse(user)
+  if (!user) {
+    return redirect('/home')
+  } else {
+    return redirect('/Orders')
+  }
+}
 const Landing = () => {
-  return <h2>Landing</h2>
+  return <></>
 }
 export default Landing
