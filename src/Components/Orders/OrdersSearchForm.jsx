51163c6048ccad49a11ed26269d33886df5b7be0
arwegasira <ac.rwegasira@gmail.com>
2025-03-20 19:52:50 +0200

    style form

--- src/Components/Orders/OrdersSearchForm.jsx
+++ src/Components/Orders/OrdersSearchForm.jsx
@@ -4,11 +4,12 @@ import FormSelect from '../Util/FormSelect'
 const orderStatus = ['', 'Pending', 'Approved', 'Canceled', 'Completed']
 const OrdersSearchForm = () => {
   return (
-    <form className=''>
+    <form className='flex flex-col gap-2 lg:flex-row lg:gap-4 lg:justify-center'>
       <FormSelect
         label='Status'
         name='orderStatus'
         list={orderStatus}
+        size='select-sm'
       ></FormSelect>
       <FormInput
         label='Order #'
@@ -16,15 +17,21 @@ const OrdersSearchForm = () => {
         name='orderNumber'
         placeholder='Order Number'
         id='room'
+        size='input-sm'
       ></FormInput>
       <FormInput
-        label='room'
+        label='Room Number'
         type='text'
         name='room'
-        placeholder='room'
+        placeholder='Room Number'
         id='room'
+        size='input-sm'
       ></FormInput>
-      <button type='submit'>Search</button>
+      <div className='mt-4 lg:flex lg:flex-col  lg:flex-grow lg:justify-center lg:mt-8'>
+        <button type='submit' className='btn btn-sm primary-button w-[100%]'>
+          Search
+        </button>
+      </div>
     </form>
   )
 }
