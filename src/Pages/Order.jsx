c6f0a2d17e34c9c5b63794738e90a04639fd85c9
arwegasira <ac.rwegasira@gmail.com>
2025-03-20 18:48:06 +0200

    infinity scrolling

--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -1,20 +1,69 @@
 import { customFetch } from '../Utils'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+import { useLoaderData } from 'react-router-dom'
+import { OrderList, Search } from '../Components'
+import { OrderSearchForm } from '../Components'
 
-const getOrderQuery = (params) => {
-  return {
-    queryKey: ['orders'],
-    queryFn: async () => await customFetch.get('/orders'),
-  }
+export const loader = async ({ request }) => {
+  let url = new URL(request.url)
+  const params = Object.fromEntries(url.searchParams)
+
+  return { params }
 }
-export const loader =
-  (queryClient) =>
-  async ({ request }) => {
-    let url = new URL(request.url)
-    const params = Object.fromEntries(url.searchParams)
-
-    return null
-  }
+const fetchOrders = async ({ pageParam, queryKey }) => {
+  const [, orderStatus, orderNumber, room] = queryKey
+  const { data } = await customFetch.get(
+    `/order?page=${pageParam}&status=${
+      orderStatus && orderStatus !== undefined ? orderStatus : ''
+    }&room=${room && room !== undefined ? room : ''}&orderNumber=${
+      orderNumber && orderNumber !== undefined ? orderNumber : ''
+    }`
+  )
+  return data
+}
 const Order = () => {
-  return <h1>Order</h1>
+  const {
+    params: { orderStatus, room, orderNumber },
+  } = useLoaderData()
+
+  const {
+    data,
+    error,
+    isFetchNextPageError,
+    isFetchingNextPage,
+    status,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryKey: ['orders', orderStatus, orderNumber, room],
+    queryFn: fetchOrders,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      lastPage.currentPage < lastPage.pageCount
+        ? lastPage.currentPage + 1
+        : undefined,
+  })
+  const { ref, inView } = useInView()
+
+  useEffect(() => {
+    if (inView) fetchNextPage()
+  }, [fetchNextPage, inView])
+
+  return (
+    <>
+      <Search>
+        <OrderSearchForm></OrderSearchForm>
+      </Search>
+      <OrderList
+        data={data}
+        error={error}
+        isFetchingNextPage={isFetchingNextPage}
+        status={status}
+        ref={ref}
+        inView={inView}
+      ></OrderList>
+    </>
+  )
 }
 export default Order
