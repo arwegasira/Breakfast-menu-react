0aa1faca5fdc922941772b042d573968cabfeefb
arwegasira <ac.rwegasira@gmail.com>
2025-03-20 19:53:51 +0200

    style as prop

--- src/Components/Util/Search.jsx
+++ src/Components/Util/Search.jsx
@@ -1,6 +1,12 @@
-const Search = ({ children }) => {
+const Search = ({ children, maxWidth, sectionPadding }) => {
   return (
-    <section className='bg-zinc-50 max-w-[50rem] mx-auto my-[-3rem] p-4  rounded-lg shadow-md'>
+    <section
+      className={`bg-zinc-50 ${
+        maxWidth ? maxWidth : 'max-w-[50rem]'
+      } mx-auto my-[-3rem] ${
+        sectionPadding ? sectionPadding : 'p-4'
+      }  rounded-lg shadow-md`}
+    >
       {children}
     </section>
   )
