2db3a4bbee7f2677626f21df1780ef2b68cf07a2
arwegasira <ac.rwegasira@gmail.com>
2025-03-20 22:34:31 +0200

    color code order status

--- src/Components/Orders/OrderList.jsx
+++ src/Components/Orders/OrderList.jsx
@@ -1,31 +1,39 @@
+import { redirect } from 'react-router-dom'
 import Loading from '../Util/Loading'
 import { forwardRef } from 'react'
 
+const statusColors = {
+  Pending: 'bg-amber-200 text-amber-800',
+  Approved: 'bg-emerald-200 text-emerald-800 ',
+  Cancelled: 'bg-red-200 text-red-800',
+  Completed: 'bg-sky-200 text-sky-800',
+}
 const OrderList = forwardRef(
   ({ data, error, isFetchingNextPage, status, inView }, ref) => {
     if (status === 'pending') return <Loading></Loading>
-    if (error)
+    if (error) {
+      if (error.status === 401) return (window.location.href = '/login')
       return (
-        <section>
-          <h2>{error.message}</h2>
+        <section className='flex justify-center items-center h-[30rem]'>
+          <h2 className='font-bold text-xl md:text-2xl leading-5'>
+            {error.message}
+          </h2>
         </section>
       )
+    }
 
     return (
       <section className='mt-16'>
         <div className='overflow-x-auto'>
-          <table className='table'>
-            {/* head */}
+          <table className='table table-xs md:table-sm lg:table-lg'>
             <thead>
-              <tr>
+              <tr className='text-sm md:text-sm lg:text-lg'>
                 <th>Order #</th>
                 <th>Room</th>
-                <th>Status</th>
-                <th>Item Count</th>
+                <th></th>
+                <th className='text-center'>Item Count</th>
               </tr>
             </thead>
-            {/* <tbody> */}
-            {/* row 1 */}
             {data?.pages?.map((page) => {
               const { currentPage, orders } = page
               return (
@@ -42,8 +50,14 @@ const OrderList = forwardRef(
                       <tr key={_id}>
                         <td>{orderNumber}</td>
                         <td>{roomDetails[0].name}</td>
-                        <td>{status}</td>
-                        <td>{orderItems.length}</td>
+                        <td className='flex justify-center'>
+                          <div
+                            className={`${statusColors[status]} p-1 rounded-md`}
+                          >
+                            {status}
+                          </div>
+                        </td>
+                        <td className='text-center'>{orderItems.length}</td>
                       </tr>
                     )
                   })}
