0bfb159564a70f2c2024dabf39f46b220368e011
arwegasira <ac.rwegasira@gmail.com>
2025-03-20 18:49:20 +0200

    input element

--- src/Components/Util/FormInput.jsx
+++ src/Components/Util/FormInput.jsx
@@ -1,4 +1,17 @@
-const FormInput = () => {
-  return <div>FormInput</div>
+const FormInput = ({ label, type, name, id, placeholder }) => {
+  return (
+    <label className='form-control w-full max-w-xs'>
+      <div className='label'>
+        <span className='label-text'>{label}</span>
+      </div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        id={id}
+        className='input input-bordered w-full max-w-xs'
+      />
+    </label>
+  )
 }
 export default FormInput
