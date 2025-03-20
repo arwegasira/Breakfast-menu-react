50d9e002128ba9e4fb8eccb4e81bf27b999d9620
arwegasira <ac.rwegasira@gmail.com>
2025-03-20 18:47:02 +0200

    order loader receives no params

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ const router = createBrowserRouter([
         path: 'Orders',
         element: <Orders></Orders>,
         errorElement: <ErrorElement></ErrorElement>,
-        loader: orderLoader(queryClient),
+        loader: orderLoader,
       },
     ],
   },
