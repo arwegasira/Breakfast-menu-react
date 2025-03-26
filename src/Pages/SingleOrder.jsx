ac7a0e0d94e88d8e54c0ed2c46e2edaeed5e9fd5
arwegasira <ac.rwegasira@gmail.com>
2025-03-26 23:38:28 +0200

    fetch single order

--- src/Pages/SingleOrder.jsx
+++ src/Pages/SingleOrder.jsx
@@ -1,8 +1,29 @@
-export const loader = async ({ params }) => {
-  console.log(params)
-  return null
+import { useLoaderData } from 'react-router-dom'
+import { customFetch } from '../Utils'
+
+const singleOderQuery = (id) => {
+  return {
+    queryKey: ['singleOrder', id],
+    queryFn: async () => customFetch.get(`/order/${id}`),
+  }
 }
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const { id } = params
+    try {
+      const response = await queryClient.ensureQueryData(singleOderQuery(id))
+      return {
+        order: response.data.order,
+      }
+    } catch (error) {
+      // un-authenticated
+      if (error.response.status === 401) return redirect('/login')
+    }
+  }
 const SingleOrder = () => {
+  const { order } = useLoaderData()
+  console.log(order)
   return <div>SingleOrder</div>
 }
 export default SingleOrder
