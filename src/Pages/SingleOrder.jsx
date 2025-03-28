ccbc886311b559876b171e074cf8fe4fee3cf3c0
arwegasira <ac.rwegasira@gmail.com>
2025-03-28 15:46:46 +0200

    add styles

--- src/Pages/SingleOrder.jsx
+++ src/Pages/SingleOrder.jsx
@@ -23,7 +23,42 @@ export const loader =
   }
 const SingleOrder = () => {
   const { order } = useLoaderData()
+  const orderStatusList = ['Pending', 'Approved', 'Cancelled', 'Completed']
+  const { orderNumber, roomDetails, status: orderStatus, orderItems } = order
   console.log(order)
-  return <div>SingleOrder</div>
+  return (
+    <>
+      <section className='mt-12 shadow rounded-lg py-8 px-4 max-w-[60rem] mx-auto'>
+        <div className='pb-4 border-b flex flex-col gap-4 sm:flex-row sm:justify-between '>
+          <span className='block text-sm px-1 font-medium md:text-base'>
+            {orderNumber}
+          </span>
+          <span className='block text-sm px-1 font-medium md:text-base'>
+            {roomDetails[0].name}
+          </span>
+          <span className='block text-sm px-1 font-medium md:text-base'>
+            {roomDetails[0].phoneNumber}
+          </span>
+          <select
+            className='select select-bordered select-sm block md:self-start md:font-medium'
+            defaultValue={orderStatus}
+          >
+            {orderStatusList.map((status, idx) => {
+              return <option key={idx}>{status}</option>
+            })}
+          </select>
+        </div>
+        <ul className='my-4 px-1 pb-4 border-b flex flex-col gap-2'>
+          {orderItems.map((item, idx) => {
+            return (
+              <li key={idx} className='text-sm md:text-base'>
+                {item}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </>
+  )
 }
 export default SingleOrder
