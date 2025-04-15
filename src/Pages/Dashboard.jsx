67a85846df924427a85c666a5eef16f37b29e7d4
arwegasira <ac.rwegasira@gmail.com>
2025-04-15 18:34:25 +0200

    dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,4 +1,33 @@
+import { RechartBar } from '../Components'
+import { customFetch } from '../Utils'
+import { redirect, useLoaderData } from 'react-router-dom'
+const dashboardQuery = () => {
+  return {
+    queryKey: ['dashboard'],
+    queryFn: async () => await customFetch.get('/order/topOrderedItems'),
+  }
+}
+export const loader = (queryClient) => async () => {
+  try {
+    const response = await queryClient.ensureQueryData(dashboardQuery())
+    return {
+      items: response.data.items,
+    }
+  } catch (error) {
+    if (error.response.status === 401) return redirect('/login')
+  }
+  return null
+}
 const Dashboard = () => {
-  return <h2>Dashboard</h2>
+  return (
+    <section className=''>
+      <h1 className='mt-4 text-xl font-medium md:text-2xl mb-8'>
+        Most Ordered Items
+      </h1>
+      {/* <div className='w-full md:w-[50%]'> */}
+      <RechartBar></RechartBar>
+      {/* </div> */}
+    </section>
+  )
 }
 export default Dashboard
