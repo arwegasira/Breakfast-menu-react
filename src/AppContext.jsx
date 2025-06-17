57ac23e76052d370a4a807ff125dd27dc582a71a
arwegasira <ac.rwegasira@gmail.com>
2025-06-17 20:47:06 +0200

    additional properties in the state

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,7 +1,12 @@
 import { createContext, useContext } from 'react'
 import { useReducer } from 'react'
 import reducer from './reducer'
-import { POPULATE_FORM_FIELDS, MANIPULATE_ORDER_ITEMS } from './actions'
+import {
+  POPULATE_FORM_FIELDS,
+  MANIPULATE_ORDER_ITEMS,
+  OPEN_MODAL,
+  CLOSE_MODAL,
+} from './actions'
 const globalContext = createContext()
 export const useGLobalContext = () => useContext(globalContext)
 
@@ -9,6 +14,7 @@ const initialSate = {
   modalTitle: '',
   orderItems: {},
   roomForm: { name: '', phoneNumber: '' },
+  isModalOpen: false,
 }
 
 const AppContext = ({ children }) => {
@@ -16,15 +22,21 @@ const AppContext = ({ children }) => {
   const openModal = () => {
     const modal = document.querySelector('.modal')
     modal.showModal()
+    return dispatch({ type: OPEN_MODAL })
   }
   const closeModal = () => {
     const modal = document.querySelector('.modal')
     modal.close()
+    return dispatch({ type: CLOSE_MODAL })
   }
   const populateFormFields = ({ fieldValues, formName, modalTitle }) => {
     return dispatch({
       type: POPULATE_FORM_FIELDS,
-      payload: { fieldValues, formName, modalTitle },
+      payload: {
+        fieldValues,
+        formName,
+        modalTitle,
+      },
     })
   }
   const manipulateOrderItems = ({ target, maxItems, category }) => {
